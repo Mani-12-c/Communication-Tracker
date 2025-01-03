81b69d124a172ba96050dc5c2da3ced30bd6c287
Mani <manikanta3790@gmail.com>
2025-01-03 17:06:03 +0530

    minor changes

--- backend/routes/company.js
+++ backend/routes/company.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Company = require('../models/Company');
+const Company = require('../models/company');
 const authMiddleware = require('../middleware/auth');
 const adminOnly = require('../middleware/admin');
 
